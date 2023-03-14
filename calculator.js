0daf144db41ffb20f3c5ccfa2fd76ca3d119eade
SINA <124919260+sinahed@users.noreply.github.com>
2023-03-13 19:48:05 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req,res){
+
+     var num1 = Number(req.body.num1)
+     var num2 = Number(req.body.num2)
+     var resualt1 =num1/(num2*num2)
+     var resualt =resualt1.toFixed(2);
+    res.send("your bmi resualt is " + resualt);
+    
+})
+
+
+app.listen(3000, function(){
+console.log("server run at 3000");
+
+});
